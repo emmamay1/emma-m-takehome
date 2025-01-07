351eb342d0e1973a22c581b0141e109a49bc2e74
Emma May <emmamay@Emmas-MacBook-Pro.local>
2025-01-06 22:26:38 -0700

    [google-auth] google sign in redirect to login working, but return to site still failing. logout button added

--- Api/Program.cs
+++ Api/Program.cs
@@ -2,13 +2,32 @@ using Microsoft.Azure.Functions.Worker;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+
 var host = new HostBuilder()
     .ConfigureFunctionsWorkerDefaults()
     .ConfigureServices(services =>
     {
         services.AddApplicationInsightsTelemetryWorkerService();
         services.ConfigureFunctionsApplicationInsights();
+        services.AddControllers();
+        services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+            .AddJwtBearer(options =>
+            {
+                options.Authority = "https://accounts.google.com";
+                options.TokenValidationParameters = new TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidIssuer = "https://accounts.google.com",
+                    ValidateAudience = true,
+                    ValidAudience = "114269276262-36tjllb91prr18qvpj1t7o40j2ccgqv1.apps.googleusercontent.com",
+                    ValidateLifetime = true
+                };
+            });
+        services.AddAuthorization();
     })
     .Build();
 
 host.Run();
+
--- Client/Program.cs
+++ Client/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.AspNetCore.Components.Web;
 using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
+using Microsoft.AspNetCore.Components.WebAssembly.Authentication;
+using Microsoft.Extensions.DependencyInjection;
 using BlazorApp.Client;
 
 var builder = WebAssemblyHostBuilder.CreateDefault(args);
@@ -7,5 +9,11 @@ builder.RootComponents.Add<App>("#app");
 builder.RootComponents.Add<HeadOutlet>("head::after");
 
 builder.Services.AddScoped(sp => new HttpClient { BaseAddress = new Uri(builder.Configuration["API_Prefix"] ?? builder.HostEnvironment.BaseAddress) });
+
+builder.Services.AddOidcAuthentication(options =>
+{
+    builder.Configuration.Bind("GoogleAuthentication", options.ProviderOptions);
+    options.ProviderOptions.DefaultScopes.Add("email");
+});
 
 await builder.Build().RunAsync();
